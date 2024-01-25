cefad3f21cbccb2a559ee39f23a48542c79e20f0
Gaspare <Gas9220@users.noreply.github.com>
2024-01-25 14:14:54 +0100

    feat: show WebView in ContentView

--- MacMarkDown/ContentView.swift
+++ MacMarkDown/ContentView.swift
@@ -11,7 +11,10 @@ struct ContentView: View {
     @Binding var document: MacMarkDownDocument
 
     var body: some View {
-        TextEditor(text: $document.text)
+        HSplitView {
+            TextEditor(text: $document.text)
+            WebView(html: document.html)
+        }
     }
 }
 
