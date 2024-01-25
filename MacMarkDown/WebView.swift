a208f44c1c63aad11a225adda9bb72e903e01f87
Gaspare <Gas9220@users.noreply.github.com>
2024-01-25 14:33:19 +0100

    feat: format html based on styleSheet

--- MacMarkDown/WebView.swift
+++ MacMarkDown/WebView.swift
@@ -9,13 +9,28 @@ import SwiftUI
 import WebKit
 
 struct WebView: NSViewRepresentable {
+    @AppStorage("styleSheet") var styleSheet: StyleSheet = .github
+
     var html: String
+
+    var formattedHtml: String {
+        return """
+          <html>
+          <head>
+             <link href="\(styleSheet).css" rel="stylesheet">
+          </head>
+          <body>
+             \(html)
+          </body>
+          </html>
+          """
+    }
 
     func makeNSView(context: Context) -> WKWebView {
         WKWebView()
     }
 
     func updateNSView(_ nsView: WKWebView, context: Context) {
-        nsView.loadHTMLString(html, baseURL: Bundle.main.resourceURL)
+        nsView.loadHTMLString(formattedHtml, baseURL: Bundle.main.resourceURL)
     }
 }
